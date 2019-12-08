bff75866e7e1084b87149a7750b7f1d330fd97f4
tsukiyoitake <tsukyotake9@gmail.com>
2019-12-08 18:05:05 +0900

    add line object

---
+++ include/vespa/line.hpp
@@ -0,0 +1,27 @@
+﻿#ifndef __VESPA_LINE__
+#define __VESPA_LINE__
+
+#include "coord.hpp"
+
+namespace vespa {
+
+    template<class type, size_t dim>
+    class line {
+    public:
+        using coord_type = coord<type, dim>;
+    public:
+        constexpr line(coord_type const& begin, coord_type end)noexcept: start_(begin), stop_(end) {}
+
+        constexpr coord_type const& start() const noexcept { return start_; }
+        constexpr coord_type& start() noexcept { return start_; }
+        constexpr coord_type const& stop() const noexcept { return stop_; }
+        constexpr coord_type& stop() noexcept { return stop_; }
+
+    private:
+        coord_type start_, stop_;
+
+    };
+
+}
+
+#endif
