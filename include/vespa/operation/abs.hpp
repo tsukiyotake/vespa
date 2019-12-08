2ef40439663d896fe2798c0a4d92fbaf7dc25fa7
tsukiyoitake <tsukyotake9@gmail.com>
2019-12-08 18:04:32 +0900

    add operation

---
+++ include/vespa/operation/abs.hpp
@@ -0,0 +1,13 @@
+﻿#ifndef __VESPA_OPERATION_ABS__
+#define __VESPA_OPERATION_ABS__
+
+namespace vespa {
+
+    template<class type>
+    constexpr type abs(type const& rhs)noexcept {
+        return rhs >= 0 ? rhs : -rhs;
+    }
+
+}
+
+#endif
